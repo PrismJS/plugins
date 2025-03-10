8d9393780c7563c85a2e634994ae430904dd65b3
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-03-10 14:57:53 +0100

    Command Line

---
+++ command-line/README.11tydata.js
@@ -0,0 +1,9 @@
+export default () => {
+	return {
+		scripts: `
+<script src="https://dev.prismjs.com/components/prism-bash.js"></script>
+<script src="https://dev.prismjs.com/components/prism-powershell.js"></script>
+<script src="https://dev.prismjs.com/components/prism-sql.js"></script>
+		`,
+	};
+};
