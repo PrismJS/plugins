586d5af5135f2d153e1b2888880eeb58c1a6b4ac
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-03-22 15:34:36 +0100

    Simplify code + use the updated 11ty API

--- command-line/README.11tydata.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-	return {
-		scripts: `
-<script src="https://dev.prismjs.com/components/prism-bash.js"></script>
-<script src="https://dev.prismjs.com/components/prism-powershell.js"></script>
-<script src="https://dev.prismjs.com/components/prism-sql.js"></script>
-		`,
-	};
-};
